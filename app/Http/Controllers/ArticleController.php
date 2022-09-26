484ec24964e2b972da2aa9ab657206793029d0ea
LNThanhNhan <20520667@gm.uit.edu.vn>
2022-09-26 11:32:38 +0700

    first commit

---
+++ app/Http/Controllers/ArticleController.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Http\Controllers;
+
+
+use App\Models\Article;
+use Illuminate\Http\Request;
+include(app_path().'/Customs/create_slug.php');
+
+class ArticleController extends Controller
+{
+    public function index()
+    {
+        $articles = Article::query()->get();
+        return view('articles.index',[
+            'articles' => $articles
+        ]);
+    }
+
+    public function create()
+    {
+        return view('articles.create');
+    }
+
+    public function store(Request $request)
+    {
+        $article=new Article();
+        $article->title = $request->get('title');
+        $article->slug=create_slug($request->get('title'));
+        $article->content = $request->get('content');
+        $article->thumbnail = $request->get('thumbnail');
+        $article->date = $request->get('created');
+        dd($article);
+        //$article->save();
+    }
+}
---
+++ routes/web.php
@@ -0,0 +1,23 @@
+<?php
+
+use App\Http\Controllers\ArticleController;
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', [ArticleController::class, 'index']);
+
+Route::get('/articles', [ArticleController::class, 'index']);
+
+Route::get('/articles/create', [ArticleController::class,'create'])->name('article-create');
+
+Route::post('/articles/create', [ArticleController::class,'store'])->name('article-store');
